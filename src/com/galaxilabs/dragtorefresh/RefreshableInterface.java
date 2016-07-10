68909574ce254c054b714717397d2e936ca553b1
Sobha Mamidi <sobmamid@cisco.com>
2016-07-10 22:52:29 +0530

    all files

---
+++ src/com/galaxilabs/dragtorefresh/RefreshableInterface.java
@@ -0,0 +1,6 @@
+package com.galaxilabs.dragtorefresh;
+
+public interface RefreshableInterface {
+	public void startFresh();
+	public void startLoadMore();
+}
---
+++ src/com/galaxilabs/rssreader/vo/PostData.java
@@ -0,0 +1,16 @@
+/**
+ * PostData.java
+ * 
+ * Value Object
+ */
+package com.galaxilabs.rssreader.vo;
+
+
+public class PostData {
+	public String postThumbUrl;
+	public String postTitle;
+	public String postDate;
+	public String postLink;
+	public String postContent;
+	public String postGuid;
+}
